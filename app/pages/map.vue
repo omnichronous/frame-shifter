499aa24d7956670b023bbdc69ee9ec0598e083d2
Mitko Georgiev <omnichronous@gmail.com>
2025-12-25 14:40:03 +0000

    feat: add map page

---
+++ app/pages/map.vue
@@ -0,0 +1,12 @@
+<template>
+  <ClientOnly>
+    <div style="height: 100vh; width: 100%">
+      <MglMap
+        map-style="https://demotiles.maplibre.org/style.json"
+        :center="[0, 0]"
+        :zoom="2"
+      />
+    </div>
+  </ClientOnly>
+</template>
+
